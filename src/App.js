8f2785cb3fe89a816c67675578dfa8a2bcdf6eda
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-12-02 16:30:32 +0800

    Implemented a view for showing a single anecdote

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import {
 	BrowserRouter as Router,
-	Switch, Route, Link
+	Switch, Route, Link,
+	useParams
 } from "react-router-dom"
 
 const Menu = () => {
@@ -21,10 +22,26 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+				<li key={anecdote.id} >
+					<Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+				</li>
+			)}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdotes }) => {
+	const id = useParams().id
+	const anecdote = anecdotes.find(a => a.id === id)
+	return (
+		<div>
+			<h2>{anecdote.content} by {anecdote.author}</h2>
+			<p>has {anecdote.votes} votes</p>
+			<p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+		</div>
+	)
+}
 
 const About = () => (
   <div>
@@ -137,6 +154,9 @@ const App = () => {
 					</Route>
 					<Route path="/about">
 						<About />
+					</Route>
+					<Route path="/anecdotes/:id">
+						<Anecdote anecdotes={anecdotes} />
 					</Route>
 					<Route path="/">
 						<AnecdoteList anecdotes={anecdotes} />
